eca4d55e9bffa1e80e5fc3e1a61679e6dfa29f38
samrullo <amrulloev.subhon@gmail.com>
2019-08-07 22:03:09 +0900

    jspanda_bot

--- botserver.py
+++ botserver.py
@@ -3,6 +3,12 @@ from flask import request
 from conf.appconf import Config
 from flask import session
 import json
+import logging
+
+FORMAT = '%(asctime)-15s %(message)s'
+logging.basicConfig(format=FORMAT, level=logging.INFO)
+_logger = logging.getLogger(__file__)
+
 from add_product_bot import handle_update
 
 app = Flask(__name__)
@@ -13,7 +19,13 @@ app.config.from_object(Config)
 def hello():
     update = request.data.decode('utf8')
     update = json.loads(update)
+    logging.info("what_to_do before : {}".format(session.get('what_to_do')))
     session['what_to_do'] = handle_update(update, session.get('what_to_do'))
+    while session.get('what_to_do'):
+        logging.info("what_to_do before : {}".format(session.get('what_to_do')))
+        session['what_to_do'] = handle_update(update, session.get('what_to_do'))
+        logging.info("what_to_do after: {}".format(session.get('what_to_do')))
+    logging.info("what_to_do after: {}".format(session.get('what_to_do')))
     return ""  # it is important as this return 200 success response to telegram
 
 
