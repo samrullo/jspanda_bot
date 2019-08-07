6b6e683023c142e1a8e5406950cbe44a5daaa912
samrullo <amrulloev.subhon@gmail.com>
2019-08-07 21:22:48 +0900

    jspanda_bot

--- botserver.py
+++ botserver.py
@@ -1,13 +1,17 @@
 from flask import Flask
 from flask import request
+import json
+from add_product_bot import handle_update
 
 app = Flask(__name__)
 
 
 @app.route('/', methods=['POST', 'GET'])
 def hello():
-    print(request.data)
-    return "" # it is important as this return 200 success response to telegram
+    update = request.data.decode('utf8')
+    update = json.loads(update)
+    handle_update(update)
+    return ""  # it is important as this return 200 success response to telegram
 
 
 if __name__ == '__main__':
