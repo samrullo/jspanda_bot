0f7068169e0441bb810d725538fde44a762fbf0e
samrullo <amrulloev.subhon@gmail.com>
2019-08-07 21:15:11 +0900

    jspanda_bot

--- botserver.py
+++ botserver.py
@@ -1,10 +1,14 @@
 from flask import Flask
+from flask import request
 
-app=Flask(__name__)
+app = Flask(__name__)
 
-@app.route('/')
+
+@app.route('/', methods=['POST', 'GET'])
 def hello():
-    return "Hello {}".format(2+2)
+    print(request.data)
+    return "" # it is important as this return 200 success response to telegram
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     app.run(host='0.0.0.0')
